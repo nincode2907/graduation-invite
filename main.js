2cecd87459918362372aa785ee6062223f278416
nincode2907 <vanninlinhcang@gmail.com>
2025-12-02 14:44:57 +0700

    fisrt version

---
+++ main.js
@@ -0,0 +1,44 @@
+const listFre = {
+    "dad": "Ba",
+    "mom": "Mẹ",
+    "sst": "Em gái",
+    "lix": "Bạn Lĩnh",
+
+    "bni": "Bạn Ni",
+    "bso": "Bạn Sơn",
+    "bth": "Bạn Thanh",
+    "bci": "Bạn Chí",
+    "bvu": "Bạn Vũ",
+    "bch": "Bạn Chỉnh",
+
+    "evy": "Em Vy",
+    "eth": "Em Thống",
+    "bnh": "Bu Nhi",
+
+    "cnj": "Công Nhật",
+    "tni": "Thảo Nhi",
+    "cma": "Cốt Mạnh",
+
+    "cka": "Chị Kim Anh",
+    "hnh": "Hà Nhi",
+    "hph": "Hữu Phú",
+
+    "phi": "Phát Hifriendz",
+
+    "a7x": "Anh 7",
+    "eng": "Em Ngọc",
+
+    "bpt": "Bạn Phướt",
+    "etr": "Em trai",
+
+    "vht": "Vợ chồng anh Hưng chị Thanh",
+
+    "edo": "Em Doanh",
+    "tph": "Thiên Phú",
+    "thn": "Thanh Ni", 
+    "epd": "Em Phát Đinh",
+    "elo": "Em Lợi",
+
+    "bho": "Bạn Hoàng",
+    "bhy": "Bạn Huy"
+};
